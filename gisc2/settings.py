ab7a8e9d3f7705691ded655d3541389a4a580058
nrwalk8939 <nwalk19@gmail.com>
2014-09-02 18:23:20 -0400

    Updated Settings, URLs, and generate_key
    In the settings file, I added the rest_framework to the installed apps, as well as set up the backend for the serializer.
    URLs, I added our first url, and the url to our API
    Generate_key, this file generates a  secret key made of random keys. I replaced the 'secret key' in the settings with a reference to the code in this file.

--- gisc2/settings.py
+++ gisc2/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.units',
+    'rest_framework'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -88,3 +89,17 @@ STATIC_URL = '/static/'
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
 )
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
+    # Use hyperlinked styles by default.
+    # Only used if the `serializer_class` attribute is not set on a view.
+    'DEFAULT_MODEL_SERIALIZER_CLASS':
+        'rest_framework.serializers.HyperlinkedModelSerializer',
+
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
